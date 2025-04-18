bebe05af8b067ec8f343bbdd3ad29e69de67b24b
Wali <keepcrow@outlook.com>
2025-04-18 22:50:25 +0800

    创建Pygame窗口

---
+++ alien_invasion.py
@@ -0,0 +1,31 @@
+import sys
+import pygame
+
+
+class AlienInvasion:
+    """ 管理游戏资源和行为的类 """
+
+    def __init__(self):
+        """ 初始化游戏并创建游戏资源 """
+        pygame.init()
+
+        # 新建一个分辨率为1200*1942的主界面
+        self.screen = pygame.display.set_mode((800, 1294))
+        pygame.display.set_caption('Alien Invasion')
+
+    def run_game(self):
+        """ 游戏主循环 """
+        while True:
+            # 监视键盘及鼠标事件
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            # 使最近的绘制可见
+            pygame.display.flip()
+        
+
+if __name__ == '__main__':
+    # 创建游戏实例并运行游戏
+    ai = AlienInvasion()
+    ai.run_game()
