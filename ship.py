c6fd68b71f01721221c2ca58d8693a4dca681bab
Wali <keepcrow@outlook.com>
2025-04-19 14:10:41 +0800

    增加飞船移动功能

--- ship.py
+++ ship.py
@@ -14,7 +14,21 @@ class Ship:
 
         # 将图片放在默认位置：屏幕底部中央
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # 移动标志
+        self.moving_right = False
+        self.moving_left = False
+
+        # 移动速度
+        self.speed = 1.
     
+    def update(self):
+        """ 根据移动标志，调整飞船位置 """
+        if self.moving_right:
+            self.rect.x += self.speed
+        elif self.moving_left:
+            self.rect.x -= self.speed
+
     def blitme(self):
         """ 在指定位置绘制飞船 """
         self.screen.blit(self.img, self.rect)
