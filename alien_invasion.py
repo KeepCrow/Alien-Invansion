c0227b7052327de13e57c965e31f33a8187050fc
Wali <keepcrow@outlook.com>
2025-04-19 15:09:37 +0800

    新增飞船射击功能

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.event import Event
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -14,7 +15,10 @@ class AlienInvasion:
 
         # 新建一个主界面
         self.screen = pygame.display.set_mode((Settings.SCREEN_WIDTH, Settings.SCREEN_HEIGHT))
+        # 新建一个飞船
         self.ship = Ship(self)
+        # 新建一个子弹组
+        self.bullets = pygame.sprite.Group()
 
         # 设置窗口标题
         pygame.display.set_caption(Settings.GAME_TITLE)
@@ -26,8 +30,18 @@ class AlienInvasion:
             self._check_events()
             # 更新飞船位置
             self.ship.update()
+            # 更新子弹的位置
+            self.bullets.update()
+            # 删除飞出屏幕的子弹
+            for bullet in self.bullets.sprites():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
             # 绘制屏幕
             self._update_screen()
+
+    def _fire(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _check_events(self):
         """ 处理键盘和鼠标事件 """
@@ -44,6 +58,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire()
 
     def _check_keyup_events(self, event: Event):
         if event.key == pygame.K_RIGHT:
@@ -54,8 +70,11 @@ class AlienInvasion:
     def _update_screen(self):
         # 设置背景色
         self.screen.fill(Settings.BACKGROUND_COLOR)
-        # 更新飞船
+        # 重绘飞船
         self.ship.blitme()
+        # 重绘所有子弹
+        for bullet in self.bullets.sprites():
+            bullet.drawme()
         # 使最近的绘制可见
         pygame.display.flip()
 
